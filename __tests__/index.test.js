f0d6f43c4b684e4a3d844c45112a307b93c60a76
SashaTolkodubova <atolkodubova@gmail.com>
2024-10-08 23:28:21 +0300

    add all

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,18 @@
 import reverse from '../src/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { expect, test } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
+  const text = readFile('data.txt').trim();
+  const expected = readFile('result.txt').trim();
+
+  expect(reverse(text)).toEqual(expected);
 });
